f7f0cbe634b2920033580e932d75b9135ac0ca02
Ian Fleeton <ianfleeton@gmail.com>
2019-01-26 20:34:37 +0000

    Update Capistrano version lock

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.2"
+lock "~> 3.11.0"
 
 set :application, "rundb"
 set :repo_url, "https://github.com/ianfleeton/rundb.git"
